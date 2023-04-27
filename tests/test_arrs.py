5b35be3e9c27919f9b2d2f941b668a533919f7b8
Dmitriy Bychkov <582620@gmail.com>
2023-04-27 16:06:42 +0800

    Add new branch and fix tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,9 +1,16 @@
+import pytest
+
 from utils import arrs
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([1, 2, 3], 1, "test") == 2
+    assert arrs.get([1, 2, 3], -3) == None
+
+
+def test_get_with_errors():
+    with pytest.raises(TypeError):
+        arrs([], 5)
 
 
 def test_slice():
